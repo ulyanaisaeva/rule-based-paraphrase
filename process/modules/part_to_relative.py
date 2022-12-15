7c590eb08a2a049d42c84fabbd65c3914f7b0140
Sophie <93821961+SofiiaUrm@users.noreply.github.com>
2022-12-15 20:07:39 +0300

    Update part_to_relative.py

--- process/modules/part_to_relative.py
+++ process/modules/part_to_relative.py
@@ -14,8 +14,8 @@ class PartToRelativeModule(ParaphraseModule):
 
     def load(self, preproc_utils: PreprocessingUtils) -> None:
     # load any tools as `preproc_utils` attributes
+        self.reproc_ultils.stanza.stanza = preproc_ultils.stanza.Pipeline('ru', processors='tokenize,pos,lemma,depparse')
         self.loaded = True
-        self.stanza = stanza.preproc_ultils.Pipeline('ru', processors='tokenize,pos,lemma,depparse')
     
     def participle_parser(self, sentence, preproc_utils: PreprocessingUtils):
         parsed_data = self.preproc_utils.stanza(sentence)
